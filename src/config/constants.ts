a6c17dab673691311d2101dfb0f4977caf5b3349
YAAQOUB KEDDAOUI <y.keddaoui@gmail.com>
2024-01-25 20:20:49 +0100

    end

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,6 +1,4 @@
-import { Role } from "src/data/types/generated";
-
-export const USERS_API_URL = process.env.REACT_APP_USERS_API_URL;
+export const API_URL = process.env.REACT_APP_API_URL;
 
 export const EMAIL_REG_EXR = /^\S+@\S+\.\S+$/g;
 export const PHONE_REG_EXR = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/g;
--- src/data/api/index.tsx
+++ src/data/api/index.tsx
@@ -1,9 +1,11 @@
 import { createApi } from "@reduxjs/toolkit/query/react";
 import { graphqlRequestBaseQuery } from "@rtk-query/graphql-request-base-query";
 
+import { API_URL } from "src/config/constants";
+
 export const api = createApi({
   baseQuery: graphqlRequestBaseQuery({
-    url: "/graphql",
+    url: `${API_URL}`,
   }),
   endpoints: () => ({}),
 });
