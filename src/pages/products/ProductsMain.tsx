5987d7cab66cb18628cf612e0ecdc25616f2305a
YAAQOUB KEDDAOUI <y.keddaoui@gmail.com>
2024-02-03 10:40:16 +0100

    file upload init

--- src/pages/products/ProductsMain.tsx
+++ src/pages/products/ProductsMain.tsx
@@ -1,9 +1,9 @@
-import React, { useCallback, useEffect, useMemo, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
 import { useAppSelector } from "src/data/store";
-import { Product } from "src/data/types/generated";
+import { CreateProductInput, Product } from "src/data/types/generated";
 import { useListProductsQuery } from "src/data/api/graphql/queries.generated";
 import { resetProducts } from "src/data/store/reducers/products";
 import { Button, Container, Stack, Typography, Box } from "src/components";
@@ -15,6 +15,9 @@ import {
 } from "src/components/Icons";
 import { ProductsTable } from "./ProductsTable";
 import { ProductsSearch } from "./ProductSearch";
+import { Input } from "@mui/material";
+import { readFile } from "src/shared/utils/files";
+import { useCreateProductMutation } from "src/data/api/graphql/mutations.generated";
 
 export interface IDashboardProps {}
 
@@ -24,16 +27,20 @@ export const ProductsMainPage = (props: IDashboardProps) => {
   const dispatch = useDispatch();
   const goTo = useNavigate();
   const { list } = useAppSelector((state) => state.products);
-  const data = useListProductsQuery({
+  const fileRef = useRef<HTMLInputElement | undefined>();
+  const [createProduct, { isLoading, isSuccess }] = useCreateProductMutation();
+  const { refetch } = useListProductsQuery({
     page,
     perPage,
   });
 
   useEffect(() => {
+    if (!isLoading && isSuccess) refetch();
+
     return () => {
       dispatch(resetProducts());
     };
-  }, []);
+  }, [isLoading, isSuccess]);
 
   const handlePageChange = useCallback(
     (_event: any, value: React.SetStateAction<number>) => {
@@ -48,6 +55,25 @@ export const ProductsMainPage = (props: IDashboardProps) => {
     },
     []
   );
+  const handleImport = () => {
+    //@ts-ignore
+    const file = fileRef.current;
+
+    file?.click();
+
+    //@ts-ignore
+    file.onchange = async ({ target }) => {
+      //@ts-ignore
+      if (target.files.length) {
+        //@ts-ignore
+        const data: CreateProductInput[] = readFile(await target.files[0].arrayBuffer());
+        const prod = data[2];
+
+        console.log(prod);
+        createProduct({ productInfo: prod });
+      }
+    };
+  };
 
   return (
     <Box
@@ -55,14 +81,19 @@ export const ProductsMainPage = (props: IDashboardProps) => {
       sx={{
         flexGrow: 1,
         py: 8,
+        pt: 0,
       }}
     >
-      <Container maxWidth="xl">
+      {" "}
+      {/* @ts-ignore //TODO: fix types */}
+      <input type="file" ref={fileRef} accept=".xlsx, .csv" hidden />
+      <Container maxWidth="xl" sx={{ mt: 0, pt: 0 }}>
         <Stack spacing={3}>
           <Stack direction="row" justifyContent="space-between" spacing={4}>
             <Stack spacing={1}>
               <Typography variant="h4">Products</Typography>
               <Stack alignItems="center" direction="row" spacing={1}>
+                <br />
                 <Button
                   color="inherit"
                   startIcon={
@@ -70,6 +101,7 @@ export const ProductsMainPage = (props: IDashboardProps) => {
                       <ArrowUpOnSquareIcon />
                     </SvgIcon>
                   }
+                  onClick={handleImport}
                 >
                   Import
                 </Button>
