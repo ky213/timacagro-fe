c2f3c5b10e49e6acba3802c5263025246b993513
YAAQOUB KEDDAOUI <y.keddaoui@gmail.com>
2024-03-01 09:05:31 +0100

    added debounce fn util

--- src/pages/users/UsersSearch.tsx
+++ src/pages/users/UsersSearch.tsx
@@ -1,33 +1,16 @@
 import MagnifyingGlassIcon from "@heroicons/react/24/solid/MagnifyingGlassIcon";
 import { Card, InputAdornment, OutlinedInput, SvgIcon } from "@mui/material";
-import { ChangeEvent, useEffect, useRef } from "react";
+import { ChangeEvent } from "react";
 import { useLazySearchUserQuery } from "src/data/api/graphql/queries.generated";
+import { debounce } from "src/shared/utils";
 
 export const UsersSearch = () => {
-  const [searchUser] = useLazySearchUserQuery();
-  const count = useRef<number>(0);
-  const timer = useRef<NodeJS.Timer>();
+  const [query] = useLazySearchUserQuery();
+  const searchUser = debounce(query, 500);
 
   const handleSearchUser = ({ target }: ChangeEvent<HTMLInputElement>) => {
-    count.current = 0;
-
-    if (!timer.current)
-      timer.current = setInterval(() => {
-        count.current += 1;
-        if (count.current > 6) {
-          searchUser({ searchQuery: target.value });
-          count.current = 0;
-          clearInterval(timer.current);
-          timer.current = undefined;
-        }
-      }, 100);
+    searchUser({ searchQuery: target.value });
   };
-
-  useEffect(() => {
-    return () => {
-      if (timer.current) clearInterval(timer.current);
-    };
-  }, []);
 
   return (
     <Card sx={{ p: 2 }}>
--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -5,3 +5,22 @@ export const getInitials = (name = "") =>
     .slice(0, 2)
     .map((v) => v && v[0].toUpperCase())
     .join("");
+
+export const debounce = (fn: Function, delay: number = 500): Function => {
+  let lastFn: Function | null = null;
+  let lastTimeout: NodeJS.Timeout;
+
+  return (...args: any) => {
+    lastFn = () => fn(...args);
+
+    const timeout = setTimeout(() => {
+      if (lastFn && lastTimeout == timeout) {
+        lastFn();
+        lastFn = null;
+      }
+      clearTimeout(timeout);
+    }, delay);
+
+    lastTimeout = timeout;
+  };
+};
